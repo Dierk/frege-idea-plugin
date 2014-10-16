67fa2cca3f512f9f3321c1cc071e212d45b09800
Dierk Koenig <dierk.koenig@canoo.com>
2014-10-16 22:22:28 +0200

    include Frege Run Configuration

---
+++ src/org/fregelang/plugin/idea/FregeRunConfigurationType.java
@@ -0,0 +1,69 @@
+package org.fregelang.plugin.idea;
+
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.ConfigurationTypeUtil;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class FregeRunConfigurationType implements ConfigurationType {
+  private final ConfigurationFactory myConfigurationFactory;
+
+  public FregeRunConfigurationType() {
+    myConfigurationFactory = new FregeFactory(this);
+  }
+
+  @Override
+  public String getDisplayName() {
+    return "Frege";
+  }
+
+  @Override
+  public String getConfigurationTypeDescription() {
+    return "Frege runnable module or script";
+  }
+
+  @Override
+  public Icon getIcon() {
+    return FregeIcons.FILE;
+  }
+
+  @Override
+  @NonNls
+  @NotNull
+  public String getId() {
+    return "FregeScriptRunConfiguration";
+  }
+
+  @Override
+  public ConfigurationFactory[] getConfigurationFactories() {
+    return new ConfigurationFactory[]{myConfigurationFactory};
+  }
+
+  public static FregeRunConfigurationType getInstance() {
+    return ConfigurationTypeUtil.findConfigurationType(FregeRunConfigurationType.class);
+  }
+
+  private static class FregeFactory extends ConfigurationFactory {
+    public FregeFactory(ConfigurationType type) {
+      super(type);
+    }
+
+    @Override
+    public boolean isApplicable(@NotNull Project project) {
+      return FileTypeIndex.containsFileOfType(FregeFileType.INSTANCE, GlobalSearchScope.allScope(project));
+    }
+
+    @Override
+    public RunConfiguration createTemplateConfiguration(Project project) {
+      return new FregeRunConfiguration("Frege Run", project, this);
+    }
+
+  }
+}
